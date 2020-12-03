bf20e0a7b20ee1c5702d0d10520cb2546128e39c
Timo Loist <timo.loist@innoq.com>
2020-12-04 00:50:45 +0100

    [Day 01] Going wild with extension methods (Maybe not required not recommended - yes, definately overkill) Still doing it for evaluation purpose and.. well just for trying it.

--- days/01/src/main/scala/Day01.scala
+++ days/01/src/main/scala/Day01.scala
@@ -1,26 +1,32 @@
 extension (input: List[Int]) {
   def product: Int = input.foldLeft(1)(_ * _)
+
+  def matchingTwoCombinations: Iterator[List[Int]] = input
+    .combinations(2)
+    .filter(_.sum == 2020)
+
+  def matchingThreeCombinations: Iterator[List[Int]] = input
+    .combinations(3)
+    .filter(_.sum == 2020)
+}
+
+extension (solutions: Iterator[List[Int]]) {
+  def print: Unit = solutions.foreach(numbers =>
+    println(s"\tSolution ${numbers.mkString(" + ")} = 2020\n\twill multiply into: ${numbers.product}")
+  )
 }
 
 def numbers = Input.asLines(TaskPart.First).map(_.toInt)
 
 object Part1 {
 
-  def main(args: Array[String]): Unit = numbers
-    .combinations(2)
-    .filter(_.sum == 2020)
-    .foreach(numbers =>
-      println(s"\tSolution ${numbers.mkString(" + ")} = 2020\n\twill multiply into: ${numbers.product}")
-    )
+  def main(args: Array[String]): Unit =
+    numbers.matchingTwoCombinations.print
 }
 
 object Part2 {
 
-  def main(args: Array[String]): Unit = numbers
-    .combinations(3)
-    .filter(_.sum == 2020)
-    .foreach(numbers =>
-      println(s"\tSolution ${numbers.mkString(" + ")} = 2020\n\twill multiply into: ${numbers.product}")
-    )
+  def main(args: Array[String]): Unit = 
+    numbers.matchingThreeCombinations.print
 
 }
