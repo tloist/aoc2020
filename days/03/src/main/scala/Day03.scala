4f57c529bc8ab80e5b6547a394e7cfddeb0f74e1
Timo Loist <timo.loist@innoq.com>
2020-12-07 23:26:34 +0100

    [Day 03] Multiple paths means more trees

--- days/03/src/main/scala/Day03.scala
+++ days/03/src/main/scala/Day03.scala
@@ -1,7 +1,21 @@
+
+val geo = Geology.parseFrom(Input.asString(TaskPart.First))
+def calculateTreesHitBy(move: Move) = geo.traceOrigin(move).count(_ == Field.Tree)
+
 object Part1 {
-  
   def main(args: Array[String]): Unit = { 
-    val treesHit = Geology.parseFrom(Input.asString(TaskPart.First)).traceOrigin(Move(3,1)).count(_ == Field.Tree)
+    val treesHit = calculateTreesHitBy(Move(3,1))
     println(s"Hit $treesHit trees on the way through this map")
   }
+}
+object Part2 {
+  def main(args: Array[String]): Unit = {
+    val product = List(Move(1,1), Move(3,1), Move(5,1), Move(7,1), Move(1,2)).map { move =>
+      val trees = calculateTreesHitBy(move)
+      println(s"Trees hit by $move are: $trees")
+      trees
+    }.foldLeft(1)(_ * _)
+    println(s"Total product is: $product")
+  }
+  
 }
