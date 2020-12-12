4281c714551234c301eaa4b44e1601045cd6bab8
Timo Loist <timo.loist@innoq.com>
2020-12-12 22:12:43 +0100

    [Day 04] Using EndoMonoidK on Kleisli to do function composition

--- days/04/src/main/scala/Day04.scala
+++ days/04/src/main/scala/Day04.scala
@@ -25,44 +25,12 @@ object Part1 {
 object Part2 {
   
   given PassportEvaluationPolicy with {
-       
-    /* Relevant documentation:
-     * * https://typelevel.org/cats/typeclasses/monoidk.html
-     * * https://typelevel.org/cats/datatypes/kleisli.html
-     * 
-     * Below is a table of some of the type class instances Kleisli can have depending on what instances F[_] has.
-     *
-     *   Type class     | Constraint on `F[_]`
-     *   -------------- | -------------------
-     *         ... other type classes ...
-     *   MonoidK*       | Monad
-     * 
-     * *These instances only exist for Kleisli arrows with identical input and output types;
-     *  that is, Kleisli[F, A, A] for some type A. These instances use Kleisli composition as the combine operation,
-     *  and Monad.pure as the empty value.
-     */
-    
-    type ValidationResultKleisli[A] = Kleisli[ValidationResult, A, A]
-    val theMonad = summon[Monad[ValidationResult]]
-    
-    /* If aboves hold true, we have a Kleisli[F, A, A] with F is ValidationResult
-     * Also we just summoned a Monad for ValidationResult
-     * => this should mean, that there is a MonadK for it, isn't it?
-     */
-    
-    // Compiles says 'no' to this one?!
-    // val _ = summon[MonoidK[ValidationResultKleisli]]
-    
-    // And if there is, there should be a 'combineAll' method for VAlidationResultKleisli[A] for all A
     
     def validate(passport: Passport): Boolean = {
-      val funcs: List[ValidationResultKleisli[Passport]] = List(validateBirthYear, validateIssueYear, validateExpirationYear, validateHeight,
-        validateHairColor, validateEyeColor, validatePassportId).map(Kleisli.apply)
-      // to reduce them, it would help to have a combineAll function available. But because of above: 'no'
-      //val combined = combineAll(funcs)
-      
-      val combined: ValidationResultKleisli[Passport] = funcs.reduce(_.andThen(_))
-      combined.run(passport).isRight
+      val combinedValidation = Kleisli.endoMonoidK[ValidationResult].algebra.combineAll(List(
+        validateBirthYear, validateIssueYear, validateExpirationYear, validateHeight, validateHairColor, validateEyeColor, validatePassportId
+      ).map(Kleisli.apply))
+      combinedValidation.run(passport).isRight
     }
   }
 
